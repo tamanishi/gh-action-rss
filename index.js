3bc5408834d2951ac4c3c7adcbeb312e5f81bd0a
Masayuki Sunahara <tamanishi@gmail.com>
2022-03-19 12:06:42 +0900

    save rss

--- index.js
+++ index.js
@@ -1,47 +1,36 @@
 import core from '@actions/core';
-import github from '@actions/github';
-import artifact from '@actions/artifact';
-const artifactClient = artifact.create();
 import Parser from 'rss-parser';
 const parser = new Parser();
 import fetch from 'node-fetch';
 import fs from 'fs';
 
+const DEFAULT_RESULT = "NOT_UPDATED";
+
 try {
   (async () => {
     const response = await fetch("https://feeds.rebuild.fm/rebuildfm");
-    const body = await response.text();
-    const feed = await parser.parseString(body);
-    core.setOutput("title", feed.title + feed.items.length);
+    const currBody = await response.text();
+    const currFeed = await parser.parseString(currBody);
+    const currCnt = currFeed.items.length;
 
-    fs.writeFileSync("./feed", body);
+    let prevCnt = -1;
+    try {
+      const prevBody = fs.readFileSync("./rss.xml", "utf-8");
+      const prevFeed = await parser.parseString(prevBody);
+      prevCnt = prevFeed.items.length;
+    } catch (e) {
 
-    // download previous text
-    // parse previous text
-    // if (previous count deffers from current count) {
-    //   setOutput("result", result);
-    //   TODO: save body text
-    //   ここで保存したファイルを後のstepでcommitする
-    // }
+    }
 
-    // feed.items.forEach(item => {
-    //   core.setOutput(item.title + ':' + item.link + ':' + item.guid);
-    // });
-    // feed 取得
-    // 取得したfeedを保存
-    // 前回と比較
-    // if (前回と個数が異なる) {
-    //   更新処理
-    // }
+    let result = DEFAULT_RESULT;
+    if (prevCnt !== currCnt) {
+      fs.writeFileSync("./rss.xml", currBody);
+      result = "UPDATED";
+    }
+    core.setOutput(result);
+
   })();
-  // // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
+  core.setOutput(DEFAULT_RESULT);
 }
